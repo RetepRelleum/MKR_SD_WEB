5a3a04166bcc89ecb505c48bb76f38f899283889
Retep Relleum <retep.relleum@bluewin.ch>
2018-09-16 08:44:53 +0200

    Reduzieren der Abfragen

--- web/js/DO.js
+++ web/js/DO.js
@@ -1,4 +1,4 @@
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -10,110 +10,115 @@ document.getElementsByTagName('head')[0].appendChild(script);
 var doArr = [];
 
 function dOSetValue() {
-    $.get("MKR/GETDIO()", function (data) {
-        var d = data.split(",");
-        d.forEach(function (item, index) {
-            if (typeof doArr[index] !== "undefined") {
-                var dO = doArr[index];
-                if (item == 0) {
-                    dO.off();
-                } else {
-                    dO.on();
-                }
-            }
-        });
+  $.get("MKR/GETDIO()", function(data) {
+    var d = data.split(",");
+    d.forEach(function(item, index) {
+      if (typeof doArr[index] !== "undefined") {
+        var dO = doArr[index];
+        if (item == 0) {
+          dO.off();
+        } else {
+          dO.on();
+        }
+      }
     });
+  });
 }
 class DO {
-    constructor(object, io, imgOn, imgOff, imgSize = "100") {
-        doArr[io] = this;
-        this.imgSize = imgSize;
-        this.io = io;
-        this.imgOff = imgOff;
-        this.imgOn = imgOn;
-        this.object = object;
-        this.object.title = "Do" + io;
-        this.getStatus();
-        this.object.onclick = this.onClick;
+  constructor(object, io, imgOn, imgOff, imgSize = "100", update = function tes() {
+    dOSetValue();
+  }) {
+    doArr[io] = this;
+    this.imgSize = imgSize;
+    this.io = io;
+    this.imgOff = imgOff;
+    this.imgOn = imgOn;
+    this.object = object;
+    this.update = update;
+    this.object.title = "Do" + io;
+    this.getStatus();
+    this.object.onclick = this.onClick;
+
+  }
+  addImgOn() {
+    var elem = document.createElement("img");
+    elem.setAttribute("src", this.imgOn);
+    elem.setAttribute("height", this.imgSize);
+    elem.setAttribute("width", this.imgSize);
+    elem.setAttribute("alt", "DoOn");
+    elem.setAttribute("id", "DoOn" + this.io);
+    document.getElementById(this.object.getAttributeNS(null, "id")).appendChild(elem);
+  }
+  addImgOff() {
+    var elem = document.createElement("img");
+    elem.setAttribute("src", this.imgOff);
+    elem.setAttribute("height", this.imgSize);
+    elem.setAttribute("width", this.imgSize);
+    elem.setAttribute("alt", "DoOff");
+    elem.setAttribute("id", "DoOff" + this.io);
+    document.getElementById(this.object.getAttributeNS(null, "id")).appendChild(elem);
+  }
+  onClick() {
+    var di = doArr[this.title.replace("Do", "")];
+    if (di.isOn()) {
+      di.setOff();
+    } else {
+      di.setOn();
     }
-    addImgOn() {
-        var elem = document.createElement("img");
-        elem.setAttribute("src", this.imgOn);
-        elem.setAttribute("height", this.imgSize);
-        elem.setAttribute("width", this.imgSize);
-        elem.setAttribute("alt", "DoOn");
-        elem.setAttribute("id", "DoOn" + this.io);
-        document.getElementById(this.object.getAttributeNS(null, "id")).appendChild(elem);
+    di.update();
+  }
+  on() {
+    var elem = document.getElementById("DoOn" + this.io);
+    if (elem) {
+      elem.parentNode.removeChild(elem);
     }
-    addImgOff() {
-        var elem = document.createElement("img");
-        elem.setAttribute("src", this.imgOff);
-        elem.setAttribute("height", this.imgSize);
-        elem.setAttribute("width", this.imgSize);
-        elem.setAttribute("alt", "DoOff");
-        elem.setAttribute("id", "DoOff" + this.io);
-        document.getElementById(this.object.getAttributeNS(null, "id")).appendChild(elem);
+    elem = document.getElementById("DoOff" + this.io);
+    if (elem) {
+      elem.parentNode.removeChild(elem);
     }
-    onClick() {
-        var di = doArr[ this.title.replace("Do", "")];
-        if (di.isOn()) {
-            di.setOff();
-        } else {
-            di.setOn();
-        }
+    this.addImgOn();
+  }
+  off() {
+    var elem = document.getElementById("DoOff" + this.io);
+    if (elem) {
+      elem.parentNode.removeChild(elem);
     }
-    on() {
-        var elem = document.getElementById("DoOn" + this.io);
-        if (elem) {
-            elem.parentNode.removeChild(elem);
-        }
-        elem = document.getElementById("DoOff" + this.io);
-        if (elem) {
-            elem.parentNode.removeChild(elem);
-        }
-        this.addImgOn();
+    elem = document.getElementById("DoOn" + this.io);
+    if (elem) {
+      elem.parentNode.removeChild(elem);
     }
-    off() {
-        var elem = document.getElementById("DoOff" + this.io);
-        if (elem) {
-            elem.parentNode.removeChild(elem);
-        }
-        elem = document.getElementById("DoOn" + this.io);
-        if (elem) {
-            elem.parentNode.removeChild(elem);
-        }
-        this.addImgOff();
-    }
-    isOn() {
-        var elem = document.getElementById("DoOn" + this.io);
-        return  (elem !== null);
-    }
-    setOn() {
-        $.get("MKR/SETDIO(" + this.io + ",1)", function (data) {
-            var nra = this.url.replace("MKR/SETDIO(", "");
-            var nr = nra.replace(",1)", "");
-            var di = doArr[ nr];
-            di.getStatus();
-        });
-    }
-    setOff() {
-        $.get("MKR/SETDIO(" + this.io + ",0)", function (data) {
-            var nra = this.url.replace("MKR/SETDIO(", "");
-            var nr = nra.replace(",0)", "");
-            var di = doArr[ nr];
-            di.getStatus();
-        });
-    }
-    getStatus() {
-        $.get("MKR/GETDIO(" + this.io + ")", function (data) {
-            var nra = this.url.replace("MKR/GETDIO(", "");
-            var nr = nra.replace(")", "");
-            var di = doArr[ nr];
-            if (data.startsWith("0")) {
-                di.off();
-            } else if (data.startsWith("1")) {
-                di.on();
-            }
-        });
-    }
+    this.addImgOff();
+  }
+  isOn() {
+    var elem = document.getElementById("DoOn" + this.io);
+    return (elem !== null);
+  }
+  setOn() {
+    $.get("MKR/SETDIO(" + this.io + ",1)", function(data) {
+      var nra = this.url.replace("MKR/SETDIO(", "");
+      var nr = nra.replace(",1)", "");
+      var di = doArr[nr];
+      di.getStatus();
+    });
+  }
+  setOff() {
+    $.get("MKR/SETDIO(" + this.io + ",0)", function(data) {
+      var nra = this.url.replace("MKR/SETDIO(", "");
+      var nr = nra.replace(",0)", "");
+      var di = doArr[nr];
+      di.getStatus();
+    });
+  }
+  getStatus() {
+    $.get("MKR/GETDIO(" + this.io + ")", function(data) {
+      var nra = this.url.replace("MKR/GETDIO(", "");
+      var nr = nra.replace(")", "");
+      var di = doArr[nr];
+      if (data.startsWith("0")) {
+        di.off();
+      } else if (data.startsWith("1")) {
+        di.on();
+      }
+    });
+  }
 }
